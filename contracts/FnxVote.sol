ecabe85c8be2b38cdc249eb6724a3f15c5e49bf9
jiaqinggang1 <forestjqg@126.com>
2020-11-21 22:31:12 +0800

    added event

--- contracts/FnxVote.sol
+++ contracts/FnxVote.sol
@@ -6,18 +6,19 @@ import "./IERC20.sol";
 import "./Ownable.sol";
 
 interface ICollateralPool {
-        function getUserInputCollateral(address user,address collateral) external view returns (uint256);
+     function getUserInputCollateral(address user,address collateral) external view returns (uint256);
 }
 
 contract FnxVote is Ownable{
     
     using SafeMath for uint256;
-
+    
+    event poolVote(uint256 indexed token,uint256 indexed uni,uint256 indexed col);
+    event allVote(address user,uint256 total);
+    
     address public fnxToken = 0xeF9Cd7882c067686691B6fF49e650b43AFBBCC6B;
     address public uniswap = 0x722885caB8be10B27F359Fcb225808fe2Af07B16;
     address public fnxCollateral = 0x919a35A4F40c479B3319E3c3A2484893c06fd7de;
-    
-    
 
     function fnxTokenBalance(address _user) public view returns (uint256) {
         return IERC20(fnxToken).balanceOf(_user);
@@ -41,11 +42,15 @@ contract FnxVote is Ownable{
        return ICollateralPool(fnxCollateral).getUserInputCollateral(_user,fnxToken);
     }
     
-    function fnxBalanceAll(address _user) public view returns (uint256) {
+    function fnxBalanceAll(address _user) public returns (uint256) {
        uint256 tokenNum = fnxTokenBalance(_user);
        uint256 uniTokenNum = fnxBalanceInUniswap(_user);
        uint256 colTokenNum = fnxBalanceInUniswap(_user);
-       return tokenNum.add(uniTokenNum).add(colTokenNum);
+       uint256 total = tokenNum.add(uniTokenNum).add(colTokenNum);
+       emit allVote(_user,total);
+       emit poolVote(tokenNum,uniTokenNum,colTokenNum);
+       
+       return total;
     }
 
     function setPools(address _fnxToken,address _uniswap,address _collateral) public onlyOwner{
