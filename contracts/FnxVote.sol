1e424bf5cae2df9856a6ced6cfa5bb49fcd30a21
jiaqinggang1 <forestjqg@126.com>
2020-11-21 21:41:04 +0800

    updated contracts

--- contracts/FnxVote.sol
+++ contracts/FnxVote.sol
@@ -3,33 +3,36 @@ pragma solidity =0.5.16;
 
 import "./SafeMath.sol";
 import "./IERC20.sol";
+import "./Ownable.sol";
 
 interface ICollateralPool {
         function getUserInputCollateral(address user,address collateral) external view returns (uint256);
 }
 
-contract FnxVote {
+contract FnxVote is Ownable{
     
     using SafeMath for uint256;
 
     address public fnxToken = 0xeF9Cd7882c067686691B6fF49e650b43AFBBCC6B;
-    address public uniswapPair = 0x722885caB8be10B27F359Fcb225808fe2Af07B16;
+    address public uniswap = 0x722885caB8be10B27F359Fcb225808fe2Af07B16;
     address public fnxCollateral = 0x919a35A4F40c479B3319E3c3A2484893c06fd7de;
+    
+    
 
     function fnxTokenBalance(address _user) public view returns (uint256) {
         return IERC20(fnxToken).balanceOf(_user);
     }
 
     function fnxBalanceInUniswap(address _user) public view returns (uint256) {
-        uint256 LpFnxBalance = IERC20(fnxToken).balanceOf(uniswapPair);
+        uint256 LpFnxBalance = IERC20(fnxToken).balanceOf(uniswap);
         if (LpFnxBalance == 0) {
             return 0;
         }
-        uint256 userLpBalance = IERC20(uniswapPair).balanceOf(_user);
+        uint256 userLpBalance = IERC20(uniswap).balanceOf(_user);
         if (userLpBalance == 0) {
             return 0;
         }
-        uint256 fnxPerUni = LpFnxBalance.mul(1e12).div(IERC20(uniswapPair).totalSupply());
+        uint256 fnxPerUni = LpFnxBalance.mul(1e12).div(IERC20(uniswap).totalSupply());
 
         return userLpBalance.mul(fnxPerUni).div(1e12);
     }
@@ -45,7 +48,11 @@ contract FnxVote {
        return tokenNum.add(uniTokenNum).add(colTokenNum);
     }
 
-
+    function setPools(address _fnxToken,address _uniswap,address _collateral) public onlyOwner{
+        fnxToken = _fnxToken;
+        uniswap = _uniswap;
+        fnxCollateral = _collateral;
+    }
 }
 
 
