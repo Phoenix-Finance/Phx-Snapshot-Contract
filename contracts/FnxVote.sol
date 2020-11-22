43991d639272afcf03bb4691355513dadeed05e9
jiaqinggang1 <forestjqg@126.com>
2020-11-22 10:52:58 +0800

    added uniMine stake cal

--- contracts/FnxVote.sol
+++ contracts/FnxVote.sol
@@ -9,6 +9,10 @@ interface ICollateralPool {
      function getUserInputCollateral(address user,address collateral) external view returns (uint256);
 }
 
+interface IUniMinePool {
+     function totalStakedFor(address addr) external view returns (uint256);
+}
+
 contract FnxVote is Ownable{
     
     using SafeMath for uint256;
@@ -19,6 +23,7 @@ contract FnxVote is Ownable{
     address public fnxToken = 0xeF9Cd7882c067686691B6fF49e650b43AFBBCC6B;
     address public uniswap = 0x722885caB8be10B27F359Fcb225808fe2Af07B16;
     address public fnxCollateral = 0x919a35A4F40c479B3319E3c3A2484893c06fd7de;
+    address public uniMine = 0x702164396De92bF0f4a1315c00EFDb5a7ea287eC;
 
     function fnxTokenBalance(address _user) public view returns (uint256) {
         return IERC20(fnxToken).balanceOf(_user);
@@ -29,13 +34,17 @@ contract FnxVote is Ownable{
         if (LpFnxBalance == 0) {
             return 0;
         }
+        
+        uint256 fnxPerUni = LpFnxBalance.mul(1e12).div(IERC20(uniswap).totalSupply());
+        
         uint256 userLpBalance = IERC20(uniswap).balanceOf(_user);
         if (userLpBalance == 0) {
             return 0;
         }
-        uint256 fnxPerUni = LpFnxBalance.mul(1e12).div(IERC20(uniswap).totalSupply());
+        
+        uint256 userUnimineLpBalance = IUniMinePool(uniMine).totalStakedFor(_user);
 
-        return userLpBalance.mul(fnxPerUni).div(1e12);
+        return (userLpBalance.add(userUnimineLpBalance)).mul(fnxPerUni).div(1e12);
     }
 
     function fnxCollateralBalance(address _user) public view returns (uint256) {
@@ -47,6 +56,7 @@ contract FnxVote is Ownable{
        uint256 uniTokenNum = fnxBalanceInUniswap(_user);
        uint256 colTokenNum = fnxBalanceInUniswap(_user);
        uint256 total = tokenNum.add(uniTokenNum).add(colTokenNum);
+       
        emit allVote(_user,total);
        emit poolVote(tokenNum,uniTokenNum,colTokenNum);
        
